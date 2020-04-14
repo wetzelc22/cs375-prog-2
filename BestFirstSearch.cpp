6453d23967d6a34eccd5e6402471516339923d2f
unknown <cwetzel2@binghamton.edu>
2020-04-14 11:48:32 -0400

    test that input read correctly and items sorted properly

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -4,21 +4,55 @@
 #include <stdio.h>
 #include <cassert>
 #include <sstream>
+#include <algorithm>
 
+struct item{
+	int weight;
+	int profit;
+};
+
+//To compare items based on p/w value
+bool compareFrac(item i1, item i2){
+	double val1 = (double)i1.profit/i1.weight;
+	double val2 = (double)i2.profit/i2.weight;
+
+	return val2 > val1;
+}
 
 int main(int argc, char ** argv){
 	//assert(argc == 3);
+
+	//read in input, put all item values into vector of item structs
 	std::ifstream reader(argv[1]);
 	std::string line;
+	int n_items, capacity;
+	int count = 0;
+	std::vector<item> items;
+	int track = 0;
 	while(std::getline(reader, line)){
 		std::stringstream linestream(line);
 		std::string value;
+		if(track == 0){
+			getline(linestream, value, ',');
+			n_items = std::stoi(value);
+			getline(linestream,value,',');
+			capacity = std::stoi(value);
+		}else{
+			int w, p;
+			std::getline(linestream, value, ',')
+			w = std::stoi(value);
+			std::getline(linestream,value, ',');
+			p = std::stoi(value);
+			items.push_back(item());
+			items[count].weight = w;
+			items[count].profit = p;
+			count++;
 
-		while(getline(linestream, value, ',')){
-			std::cout << "Value = " << value << std::endl;
 		}
-		std::cout << "End of line==============" << std::endl;
 	}
+
+	std::sort(items.begin(), items.end(), compareFrac);
+
 
 
 
